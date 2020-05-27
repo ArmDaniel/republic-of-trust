98e12d32aa42219308d5667acd73c7bbb69d2181
Mr. Motanovici <44145743+Motanovici@users.noreply.github.com>
2020-05-28 00:25:04 +0300

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,3 +3,11 @@ var Arg = "Hello world";
 module.exports = deployer => {
     deployer.deploy(HelloBlockchain, Arg);
 };
+
+var Voting = artifacts.require("Voting");
+//expects bytes32 value, must add
+//var Arg2 = ...
+
+module.exports = deployer => {
+  deployer.deploy(Voting,Arg2);
+};
